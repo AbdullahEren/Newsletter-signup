e3c100f7d451a56ac79fde7f9c9a8fabddc45d49
abdullaheren <a.eyurek@hotmail.com>
2023-03-04 17:53:37 +0300

    Uploading Newsletter files

---
+++ app.js
@@ -0,0 +1,64 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+const https = require("https");
+const app = express();
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.listen(process.env.PORT || 3000,function(req,res){
+  console.log("Server is running on port 3000.");
+})
+
+app.get("/",function(req,res){
+  res.sendFile(__dirname+"/signup.html");
+
+})
+
+app.post("/",function(req,res){
+  const firstName = req.body.fName;
+  const lastName = req.body.lName;
+  const email = req.body.email;
+  const data = {
+    members:[
+      {
+        email_address:email,
+        status:"subscribed",
+        merge_fields:{
+          FNAME: firstName,
+          LNAME: lastName
+        }
+      }
+    ]
+  };
+  const url = "https://us21.api.mailchimp.com/3.0/lists/42c49ccb07"
+
+  const options = {
+    method:"POST",
+    auth:"AbdullahEren:621a0e057124213fa5dfa1fa1e65cd19-us21"
+  }
+  const jsonData = JSON.stringify(data);
+
+  const request = https.request(url,options,function(response){
+
+    if(response.statusCode===200){
+      res.sendFile(__dirname+"/success.html");
+    }else{
+      res.sendFile(__dirname+"/failure.html");
+    }
+
+    response.on("data",function(data){
+      console.log(JSON.parse(data));
+    });
+  });
+  request.write(jsonData);
+  request.end();
+
+})
+
+app.post("/failure.html",function(req,res){
+  res.redirect("/")
+})
+var apiKey = "621a0e057124213fa5dfa1fa1e65cd19-us21";
+var listId = "42c49ccb07";
